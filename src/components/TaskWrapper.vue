450d533af08cde7a50e9493c07f46727ec6caf87
Rafael <rafael.estevamdesign@gmail.com>
2022-09-08 13:43:35 -0300

    list

---
+++ src/components/TaskWrapper.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    {{ status }}
+    <v-card
+      v-for="task in tasksList"
+      :key="task.id"
+    >
+      {{ task.name }}
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TaskWrapperVue',
+  props: {
+    status: String,
+    tasks: Array,
+  },
+  data() {
+    return {
+      tasksList: [],
+    };
+  },
+  async created() {
+    this.tasksList = this.tasks;
+  },
+};
+</script>
